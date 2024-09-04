5ee8ea2caec0ac6a1d4040d87fa2eb4b8b4a1aa0
gabin <gabin.chognot@uqac.ca>
2024-09-04 17:54:04 -0400

    Add all files

--- Jetpack_Compose_Text/app/src/main/java/com/example/jetpack_compose_text/MainActivity.kt
+++ Jetpack_Compose_Text/app/src/main/java/com/example/jetpack_compose_text/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -101,7 +102,7 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
 fun GreetingPreview() {
     Jetpack_Compose_TextTheme {
         GreetingImage(
-            message = "Happy Birthday Gabin!",
+            message = stringResource(R.string.happy_birthday_text),
             from = "From Gabin"
         )
     }
