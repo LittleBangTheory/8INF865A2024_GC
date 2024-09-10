cb9970d1b78e5614bf25661dea65a8c1371003f7
gabin <gabin.chognot@uqac.ca>
2024-09-10 15:44:40 -0400

    add next and done buttons on keyboard

--- Tip_Calculator_Starter/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Tip_Calculator_Starter/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -43,6 +43,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -94,6 +95,10 @@ fun TipTimeLayout() {
         )
         EditNumberField(
             label = R.string.bill_amount,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
             value = amountInput,
             onValueChange = {amountInput = it},
             modifier = Modifier
@@ -110,6 +115,10 @@ fun TipTimeLayout() {
         */
         EditNumberField(
             label = R.string.how_was_the_service,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
             value = tipInput,
             onValueChange = {tipInput = it},
             modifier = Modifier
@@ -145,6 +154,7 @@ fun TipTimeLayoutPreview() {
 @Composable
 fun EditNumberField(
     @StringRes label: Int,
+    keyboardOptions: KeyboardOptions,
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
@@ -154,7 +164,7 @@ fun EditNumberField(
         onValueChange = onValueChange,
         singleLine = true,
         label = {Text(stringResource(label))},
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardOptions = keyboardOptions,
         modifier = modifier
     )
 }
