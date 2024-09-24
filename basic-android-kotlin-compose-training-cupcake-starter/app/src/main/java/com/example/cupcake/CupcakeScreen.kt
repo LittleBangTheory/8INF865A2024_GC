e79ede773c8d5e42e498b16ef7e4d69e316329d3
gabin <gabin.chognot@uqac.ca>
2024-09-24 18:21:18 -0400

    access to other app

--- basic-android-kotlin-compose-training-cupcake-starter/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ basic-android-kotlin-compose-training-cupcake-starter/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -15,6 +15,8 @@
  */
 package com.example.cupcake
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -139,9 +141,13 @@ fun CupcakeApp(
             }
 
             composable(route = CupcakeScreen.Summary.name) {
+                val context = LocalContext.current
                 OrderSummaryScreen(
                     orderUiState = uiState,
-                    onSendButtonClicked = {subject : String, summary : String -> },
+                    onSendButtonClicked = {
+                        subject : String, summary : String ->
+                        shareOrder(context, subject = subject, summary = summary)
+                    },
                     onCancelButtonClicked = { cancelOrderAndNavigateToStart(viewModel, navController) },
                     modifier = Modifier.fillMaxHeight()
                 )
@@ -151,6 +157,27 @@ fun CupcakeApp(
     }
 }
 
+private fun shareOrder(
+    context: Context,
+    subject: String,
+    summary: String
+){
+    val intent = Intent(Intent.ACTION_SEND).apply{
+        type = "text/plain"
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TEXT, summary)
+    }
+    context.startActivity(
+        Intent.createChooser(
+            intent,
+            context.getString(R.string.new_cupcake_order)
+        )
+    )
+}
+
+/**
+ * Resets the [OrderViewModel] and pops up to [CupcakeScreen.Start]
+ */
 private fun cancelOrderAndNavigateToStart(
     viewModel: OrderViewModel,
     navController: NavHostController
